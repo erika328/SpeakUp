25a0c55675f2d69d5b9b301fe9436f03db30d005
Erika Watanabe <erika03281996@yahoo.co.jp>
2024-05-20 11:56:19 +0900

    アプリ全体の色を変更、微調整

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,7 +1,7 @@
 class VideosController < ApplicationController
   def index
     @q = Video.ransack(params[:q])
-    @videos = @q.result(distinct: true).page(params[:page]).per(12)
+    @videos = @q.result(distinct: true).page(params[:page]).per(16)
   end
 
   def show
