dc60cfccd77197a3205d2b5798f91dc76d9c5be4
Erika Watanabe <erika03281996@yahoo.co.jp>
2024-05-27 12:50:57 +0900

    単語読み上げ機能

--- app/javascript/modal.js
+++ app/javascript/modal.js
@@ -9,6 +9,7 @@ function showWordModal(element) {
     <div class="font-bold text-lg text-center">
       <div>
         <label for="english_word_${wordId}" class="px-1">English</label>
+        <i class="fa-solid fa-volume-high fa-lg" id="speak_english_${wordId}"></i>
         <p id="english_word_${wordId}" class="block w-full px-5 py-3 border border-transparent rounded-lg bg-gray-50">${wordEnglish}</p>
       </div>
       <div>
@@ -23,9 +24,22 @@ function showWordModal(element) {
     </div>
   `;
 
+  // 読み上げボタンにイベントリスナーを追加
+  const speakButton = document.getElementById(`speak_english_${wordId}`);
+  speakButton.addEventListener('click', function() {
+    speakText(wordEnglish);
+  });
+
   // モーダルを表示する
   const modal = document.getElementById(`my_modal_${wordId}`);
   modal.showModal();
+}
+
+// テキストを読み上げる関数
+function speakText(text) {
+  const utterance = new SpeechSynthesisUtterance(text);
+  utterance.lang = 'en-UK'; // 読み上げる言語を設定
+  speechSynthesis.speak(utterance);
 }
 
 // document.addEventListener('DOMContentLoaded', setup);
