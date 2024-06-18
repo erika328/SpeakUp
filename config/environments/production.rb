3f595a593f7ed74e393fd54e99138431b3b8a9d2
Erika Watanabe <erika03281996@yahoo.co.jp>
2024-06-18 21:20:39 +0900

    password reset

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,12 +73,12 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   
-  config.action_mailer.default_url_options = { host: 'https://speakup.fly.dev/' }
+  config.action_mailer.default_url_options = { host: 'https://speakup-app.com/' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'speakup.fly.dev',
+    domain:               'speakup-app.com',
     user_name:            ENV['SMTP_USERNAME'],
     password:             ENV['SMTP_PASSWORD'],
     authentication:       'plain',
