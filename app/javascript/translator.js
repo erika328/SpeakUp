166156bcb0cc8397acd2d9f836af9ff2e5c898a5
Erika Watanabe <erika03281996@yahoo.co.jp>
2024-07-04 21:51:08 +0900

    翻訳機能のボタン追加

--- app/javascript/translator.js
+++ app/javascript/translator.js
@@ -1,30 +1,48 @@
 document.addEventListener('DOMContentLoaded', () => {
   const container = document.querySelector('.transcript');
+  const translateButton = document.getElementById('translate-button');
   let selectionTimeout;
 
   const handleSelection = () => {
     clearTimeout(selectionTimeout);
     selectionTimeout = setTimeout(() => {
       const selectedText = window.getSelection().toString().trim();
-      
-      if (selectedText && selectedText.split(/\s+/).length === 1) {
-        fetch(`/words/translate_text?text=${selectedText}`)
-          .then(response => response.json())
-          .then(data => {
-            if (data.translated_text) {
-              openModalWithWord(selectedText, data.translated_text);
-            } else {
-              alert('Translation failed: ' + data.error);
-            }
-          });
-      } else if (selectedText) {
-        alert('Please select only one word.');
+
+      if (selectedText) {
+        translateButton.classList.remove('hidden');
+
+        translateButton.onclick = () => {
+          if (selectedText.split(/\s+/).length > 1) {
+            alert('Please select only one word.');
+          } else {
+            fetch(`/words/translate_text?text=${selectedText}`)
+              .then(response => response.json())
+              .then(data => {
+                if (data.translated_text) {
+                  openModalWithWord(selectedText, data.translated_text);
+                } else {
+                  alert('Translation failed: ' + data.error);
+                }
+              });
+          }
+        };
+      } else {
+        translateButton.classList.add('hidden');
       }
     }, 500); // 500msの遅延を設定して、ドラッグが完了するのを待つ
   };
 
+  const handleMouseUp = () => {
+    const selectedText = window.getSelection().toString().trim();
+
+    if (!selectedText) {
+      translateButton.classList.add('hidden');
+    }
+  };
+
   container.addEventListener('mouseup', handleSelection);
   container.addEventListener('touchend', handleSelection);
+  document.addEventListener('click', handleMouseUp);
 
   function openModalWithWord(word, meaning) {
     const englishWordInput = document.querySelector('input[id="english_word"]');
